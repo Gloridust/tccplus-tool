65952904684ea10ded0821c1d3b4ff504f2706db
Gloridust <Gloridust@gmail.com>
2024-04-12 23:09:23 +0800

    Update tccplus-tool.py

--- tccplus-tool.py
+++ tccplus-tool.py
@@ -262,7 +262,7 @@ def get_bundle_identifier(app_path):
         print("Invalid Info.plist file")
 
 def run_tccplus(action,service,bundle_ident):
-    command = ["~/Library/Caches/tccplus", action, service, bundle_ident]
+    command = ["~/Library/Caches/tccplus-tool/tccplus", action, service, bundle_ident]
     try:
         result = subprocess.run(command, capture_output=True, text=True, check=True)
         print(result.stdout)
